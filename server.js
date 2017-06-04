35b8199d0ab720c9ef37d41f4d919b580990ee83
Dana Ng <ngdana1@gmail.com>
2017-06-04 02:43:05 -0700

    updated with working react app w/o html webpack plugin

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname, +'/public'));
+app.use(express.static('public'))
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve('public', 'index.html'));
-// });
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+})
+
 
 app.listen(5000, () => {
     console.log(`app is running on port 5000`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 const path = require('path');
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './public/index.html',
-    filename: 'index.html',
-    inject: 'body'
-});
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './client/index.js',
+    entry: path.resolve(__dirname, 'client/index.js'),
     output: {
-        path: path.resolve('dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+        publicPath: '/public'
     },
     module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+        loaders: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
             { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
             {
                 test: /\.scss$/,
                 loader: 'style-loader!css-loader!sass-loader'
             }
         ]
-    },
-    plugins: [HtmlWebpackPluginConfig]
+    }
 }
